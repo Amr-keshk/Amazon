cde4792780360deef5132e2c322a1a06d3b74534
Amrkeshk <120935703+Amr-keshk@users.noreply.github.com>
2023-12-29 20:12:20 +0200

    Add files via upload

---
+++ discuss.js
@@ -0,0 +1,31 @@
+//Git => lets us track changes in our code and it helps us easily undo our changes
+// Repository => is a folder where changes are being tracked
+
+// by clicking initialized repository. this turns the current folder into repository and now all the changes inside this folder will be tracked by Git
+
+
+// the next step is to configure a username and email with Git 
+
+/*Main Idea of JavaScript
+1. Save the data , data => means information , in this project information about our product which is the name, price, rating and image
+2. Generate the HTML
+3. Make it interactive
+*/
+
+
+/*
+  (Number to convert).fixed(number of decimals) this method convert the number into string but we can tell it how many decimals we want between the brackets
+*/
+
+/** what we learned
+ * 1. Start the final Amazon project
+ * 2. Set up and learned Git
+ * 3. Learned the main ideaa of java script 
+ *    1) Save the data 
+ *    2) Generate the HTML
+ *    3) Make it interactive
+ * 4. Created list of products
+ * 5. Made the "Add to cart" button interactive
+ * 6. Made the cart quantity interactive
+ * 
+ */
